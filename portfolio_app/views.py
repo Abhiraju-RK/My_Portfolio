430bf6acde885f41556d1bc2a6b6ca8df9a48f32
Abhiraju R.K <abhiraju5984@gmail.com>
2025-03-28 09:24:34 +0530

    Add files via upload

---
+++ portfolio_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from . models import Contact
+from django.http import FileResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def contact(request):
+    if request.method  == "POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        message=request.POST['message']
+        
+        Contact.objects.create(name=name,email=email,message=message)
+        return redirect('home')
+    return render(request, 'index.html')
+
+def download_resume(request):
+    file_path='static/ABHIRAJU Resume.pdf (1).pdf'
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename="My_Resume.pdf")
